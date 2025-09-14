e24c4599738056eeabf9f1acb891f434bcb94dfd
Anburut <asefabiniam851@gmail.com>
2025-09-14 17:49:51 +0300

    packge.json updated

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/clone_project/", // your repo name
+  base: "/clone_project_2025/", // your repo name
   plugins: [react()],
 });
