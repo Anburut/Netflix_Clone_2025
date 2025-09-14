1f7ac4ac3fef3026671e694ca9657e7bdce88f16
Anburut <asefabiniam851@gmail.com>
2025-09-14 14:39:05 +0300

    git pages_name_update

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Netflix_Clone_2025/", // your repo name
+  base: "/movieStreamHub/", // your repo name
   plugins: [react()],
 });
