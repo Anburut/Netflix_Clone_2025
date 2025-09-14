5f9c2190d0172806e9616bdd64a128f647758082
Anburut <asefabiniam851@gmail.com>
2025-09-14 15:22:52 +0300

    renamed repo

---
+++ movie-stream/src/Pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner.'
+import Rows_List from '../../Components/Rows/Rows_List/Rows_List'
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <Banner />
+    <Rows_List />
+    <Footer />
+    </>
+    
+
+
+  )
+}
+
+export default Home
--- netflix-clone/src/Components/Footer/Footer.jsx
+++ netflix-clone/src/Components/Footer/Footer.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import './Footer.css'
+import React from "react";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <div className='footer_outer_container'>
-      <div className='footer_inner_container'>
-        <div className='footer_icons'>
-          <FacebookOutlinedIcon />
-          <InstagramIcon />
-          <YouTubeIcon />
+    <footer className="footer_outer_container">
+      <div className="footer_inner_container">
+        {/* Social Icons */}
+        <div className="footer_icons">
+          <a href="#" aria-label="Facebook">
+            <FacebookOutlinedIcon />
+          </a>
+          <a href="#" aria-label="Instagram">
+            <InstagramIcon />
+          </a>
+          <a href="#" aria-label="YouTube">
+            <YouTubeIcon />
+          </a>
         </div>
-        <div className='footer_data'>
-          <div>
-            <ul>
-              <li>Audio Description</li>
-              <li>Legal Notice</li>
-              <li>Investor Relations</li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>Help Center</li>
-              <li>Media Center</li>
-              <li>Jobs</li>
-           </ul>
-          </div>
-          <div>
-            <ul>
-                <li>Terms of Use</li>
-              <li>Cookie Preferences</li>
-              <li>Contact Us</li> 
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
-export default Footer
+        {/* Footer Links */}
+        <div className="footer_links">
+          <ul>
+            <li>Audio Description</li>
+            <li>Legal Notice</li>
+            <li>Investor Relations</li>
+          </ul>
+          <ul>
+            <li>Help Center</li>
+            <li>Media Center</li>
+            <li>Jobs</li>
+          </ul>
+          <ul>
+            <li>Terms of Use</li>
+            <li>Cookie Preferences</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+
+        <p className="footer_copyright">
+          &copy; {new Date().getFullYear()} YourCompany. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
