77053f0927f735025862104588e22824034be7b7
Anburut <asefabiniam851@gmail.com>
2025-09-14 18:22:30 +0300

    Delete movie-stream directory

--- movie-stream/src/utils/axios.jsx
+++
@@ -1,9 +0,0 @@
-// axios.jsx
-import axios from "axios";
-
-// Define baseURL for all TMDB requests
-const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3", // TMDB base URL
-});
-
-export default instance;
